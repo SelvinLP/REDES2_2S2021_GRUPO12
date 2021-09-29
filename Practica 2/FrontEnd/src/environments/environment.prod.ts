2bab95e9ec66c2ad9345aafa1620a057cd3919b9
Selvin Lisandro <selvin.lisandro@gmail.com>
2021-09-28 21:20:03 -0600

    Limpiando un poco la plantilla

--- Practica 2/FrontEnd/src/environments/environment.prod.ts
+++ Practica 2/FrontEnd/src/environments/environment.prod.ts
@@ -6,11 +6,11 @@ export const environment = {
   apiUrl: 'api',
   appThemeName: 'Metronic',
   appPurchaseUrl: 'https://1.envato.market/EA4JP',
-  appHTMLIntegration: 'https://preview.keenthemes.com/metronic8/demo1/documentation/base/helpers/flex-layouts.html',
-  appPreviewUrl: 'https://preview.keenthemes.com/metronic8/angular/demo1/',
-  appPreviewAngularUrl: 'https://preview.keenthemes.com/metronic8/angular/demo1',
-  appPreviewDocsUrl: 'https://preview.keenthemes.com/metronic8/angular/docs',
+  appHTMLIntegration: 'https://preview.keenthemes.com/Metronic8/demo1/documentation/base/helpers/flex-layouts.html',
+  appPreviewUrl: 'https://preview.keenthemes.com/Metronic8/angular/demo1/',
+  appPreviewAngularUrl: 'https://preview.keenthemes.com/Metronic8/angular/demo1',
+  appPreviewDocsUrl: 'https://preview.keenthemes.com/Metronic8/angular/docs',
   appPreviewChangelogUrl:
-    'https://preview.keenthemes.com/metronic8/angular/docs/changelog',
+    'https://preview.keenthemes.com/Metronic8/angular/docs/changelog',
   // apiUrl: 'mysite.com/api'
 };
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Redes2
+# Laboratorio de Redes de Sitemas 2
 Repositorio para Proyectos y Practicas del curso de Redes de Sistemas 2
+
+## Integrantes
+Integrantes de desarrollo
+| Nombre | Carnet |
+|:--------------|:-------------:|
+| Selvin Lisandro Aragón Pérez | 201701133 | 
+
+
+***
+## Tabla de Prácticas y Proyectos
+1. [Proyecto Fase1](https://github.com/SelvinLP/REDES2_2S2021_GRUPO12/blob/main/Practica%201/Enunciado/%5BRedes2%5DPractica1.pdf)
+2. [Proyecto Fase2](https://github.com/SelvinLP/REDES2_2S2021_GRUPO12/blob/main/Practica%202/%5BRedes2N%5DPractica2.pdf)
