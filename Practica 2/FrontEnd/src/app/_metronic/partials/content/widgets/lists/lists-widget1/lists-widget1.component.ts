27d252eb4309a25a96eeb255ce41e1a899833da3
andreadsaenz <62906020+andreadsaenz@users.noreply.github.com>
2021-10-03 18:15:51 -0600

    dockerfile fail

--- Practica 2/FrontEnd/src/app/_metronic/partials/content/widgets/lists/lists-widget1/lists-widget1.component.ts
+++ Practica 2/FrontEnd/src/app/_metronic/partials/content/widgets/lists/lists-widget1/lists-widget1.component.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-lists-widget1',
   templateUrl: './lists-widget1.component.html',
 })
 export class ListsWidget1Component {
-  constructor() {}
+  constructor(private router: Router) {}
+
+  public newReport = {
+    Carnet: "",
+    Nombre: "",
+    Proyecto: "",
+    Servidor: "..",
+    Fecha: Date.now(),
+    Cuerpo: ""
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  guardarReporte(){
+    console.log(this.newReport)
+  }
+
 }
---
+++ Practica 2/FrontEnd/src/app/modules/auth/services/reporte.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Observable, BehaviorSubject, of, Subscription } from 'rxjs';
+import { map, catchError, switchMap, finalize } from 'rxjs/operators';
+import { UserModel } from '../models/user.model';
+import { AuthModel } from '../models/auth.model';
+import { AuthHTTPService } from './auth-http';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+export type UserType = UserModel | undefined;
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class reporteService {
+    constructor(private http:HttpClient){}
+
+}
